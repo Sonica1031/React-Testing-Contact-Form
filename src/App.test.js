b139632d24f54af5346ad9d1fdeb52596921e29f
Sonica1031 <53800242+Sonica1031@users.noreply.github.com>
2020-02-22 13:09:08 -0800

    I don't know what else to add

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,19 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { ContactForm } from './components/ContactForm';
+import { render, findAllByAltText } from "@testing-library/react";
 import App from "./App";
+import {onSubmit} from "./components/ContactForm";
 
 test("renders App without crashing", () => {
   render(<App />);
+
+});
+
+test("Inputs are rendering", () => {
+  const { getByText } = render(<App />);
+
+  getByText(/First Name/i);
+  getByText(/Last Name/i);
+  getByText(/Email/i);
+  getByText(/Message/i);
 });
